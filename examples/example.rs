476247a2f07000692159ad696a2b108ae4769a90
O2s0k0i3 <elultimopekka@gmail.com>
2024-02-05 23:15:11 +0100

    LinearFit

--- examples/example.rs
+++ examples/example.rs
@@ -1,5 +1,4 @@
-use ferrilab as fl;
-use fl::{measure, Measure, reader::read_to_measures, tables::typst, fit::linear_fit};
+use ferrilab::{measure, LinearFit, Measure, Reader, Table}; 
 fn main() {
     // First let's create some measures manually from data obtained in laboratory.
     let time = measure!([0.0001346, 0.1134, 0.22734, 0.312324, 0.4019256, 0.5127634], [0.0000123, 0.0154, 0.012, 0.02943, 0.02544, 0.04872]; true);
@@ -7,7 +6,7 @@ fn main() {
     let position = measure!([0.0023, 1.41134, 2.425, 3.41515, 5.13545, 7.24524], [0.000123, 0.154, 0.2, 0.43, 0.544, 0.872]; true);
 
     // Or use the reader module to extract data from a file.
-    let data = read_to_measures("data.txt", "\t", "\n", ",", 0).unwrap();
+    let data = Reader::new("data.txt", 0).read_to_measures();
 
     let _time = data[0].clone();
     let _position = data[1].clone();
@@ -17,17 +16,16 @@ fn main() {
 
     // Let's create a graph to visualize how speed changes over time.
     // Before the graph we have to fit the data.
-    let (slope, intercept) = linear_fit(&time, &position);
+    let (slope, intercept) = LinearFit::new(&time, &position).fit();
 
     // not plot module yet :(
 
     // Create a table for your favourite report writer tool (latex or typst supported right now)
     println!(
         "{}",
-        typst(
+        Table::new(
             vec![time, position, speed],
             vec!["t/s", "x/m", "v/ms^(-1)"],
-            true
-        )
+        ).typst()
     )
 }
